6b53ad67a3f5ca8ea978affe9479e802751eb658
Ivo Kostovski <ivo.kostovski@gmail.com>
2018-10-21 19:58:31 +0200

    small buggs fixed

--- mk-words-game/src/app/game/game-input/game-input.component.ts
+++ mk-words-game/src/app/game/game-input/game-input.component.ts
@@ -17,7 +17,7 @@ export class GameInputComponent implements OnInit {
   submitedWord: Word;
   rForm: FormGroup;
   isValid: boolean;
-  timer = 10;
+  timer = 60;
 
   constructor(private fb: FormBuilder) {
     this.rForm = fb.group({
